e801e341b622da70867deab3b45499973e729209
Grayce Muthui <graycemuthui20@gmail.com>
2023-01-10 10:18:14 +0300

    Add a checkbox

--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,16 @@ const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
 const form = document.querySelector("form");
 const list = document.getElementById("todolist");
-// btn.addEventListener("click", function () {
-//   alert("CLICKED");
-// });
+const todos = [];
 const handleSubmit = (e) => {
     e.preventDefault();
     const newTodoText = input.value;
     const newLi = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
     newLi.append(newTodoText);
+    newLi.append(checkbox);
     list.append(newLi);
-    input.value = " ";
+    input.value = "";
 };
 form.addEventListener("submit", handleSubmit);
--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,17 @@ const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("form")!;
 const list = document.getElementById("todolist")!;
 
-// btn.addEventListener("click", function () {
-//   alert("CLICKED");
-// });
-
+const todos = [];
 const handleSubmit = (e: SubmitEvent) => {
   e.preventDefault();
   const newTodoText = input.value;
   const newLi = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
   newLi.append(newTodoText);
+  newLi.append(checkbox);
   list.append(newLi);
-  input.value = " ";
+  input.value = "";
 };
 
 form.addEventListener("submit", handleSubmit);
